7547d2c9eed361bcc4720017a911fcd7343a94ae
DESKTOP-HVP6MPC\Chris <chris@thecarmans.com>
2017-06-07 21:44:22 +0200

    Error handling

--- Projekt.py
+++ Projekt.py
@@ -13,9 +13,6 @@ subdirs = [x[1] for x in os.walk(cwd+'\data')][0]
 
 #Importing all the datasets with the function fileImport, reference the dataset with table['file']
 #No extension in file.
-
-
 for x in subdirs:
-	table.update(fileImport(os.getcwd()+'\\data\\'+x,'.txt', skip_head = 19, skip_foot= 2, delimiter ='\t'))
+	table.update(fileImport(os.getcwd()+'\\data\\' + x,'.txt', skip_head = 19, skip_foot= 2, delimiter ='\t'))
 
-print(len(table))
--- fileImport.py
+++ fileImport.py
@@ -5,7 +5,7 @@ def fileImport(directory, ext ,delimiter,skip_head=0,skip_foot=0):
 	#global table
 	table= {}
 	files = os.listdir(directory)
-
+	nanCounter = 0
 	if skip_head is None:
 		skip_header = 0
 	if skip_head is None:
@@ -13,6 +13,20 @@ def fileImport(directory, ext ,delimiter,skip_head=0,skip_foot=0):
 
 	for x in files:
 		if ext in x:
-			table[x[:-len(ext)]] = np.genfromtxt(directory+'\\'+x, delimiter = delimiter, skip_header = skip_head, skip_footer = skip_foot)
-	print('All Files in ' + directory + ' has been imported')
+			try:
+				table[x[:-len(ext)]] = np.genfromtxt(directory+'\\'+x, delimiter = delimiter, skip_header = skip_head, skip_footer = skip_foot)
+			except:
+				print('Some error occured while doing stuff to '  + x)
+				
+		try:		
+			if 'nan' in str(table[x[:-len(ext)]]):
+				print('Something is returning nan in ' + x)
+				nanCounter += 1
+		except KeyError:
+			print('KeyError ' + x)
+
+	if len(table) == len(files) and nanCounter == 0 :
+		print('All functions in ' + directory + ' has been imported')
+
+
 	return table
