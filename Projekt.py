f2be3a7804da3424b2d98488fe53a7568920eb1b
DESKTOP-HVP6MPC\Chris <chris@thecarmans.com>
2017-06-08 00:31:23 +0200

    Something

--- Projekt.py
+++ Projekt.py
@@ -8,11 +8,13 @@ from fileImport import fileImport
 table ={}
 
 cwd = os.getcwd()
-subdirs = [x[1] for x in os.walk(cwd+'\data')][0]
+dataDir = cwd+'\\data\\'
+subdirs = [x[1] for x in os.walk(dataDir)][0]
 
 
 #Importing all the datasets with the function fileImport, reference the dataset with table['file']
 #No extension in file.
 for x in subdirs:
-	table.update(fileImport(os.getcwd()+'\\data\\' + x,'.txt', skip_head = 19, skip_foot= 2, delimiter ='\t'))
+	table.update(fileImport(dataDir + x,'.txt', skip_head = 19, skip_foot= 2, delimiter ='\t'))
 
+print(len(table))
--- fileImport.py
+++ fileImport.py
@@ -2,22 +2,16 @@ import os
 import numpy as np
 
 def fileImport(directory, ext ,delimiter,skip_head=0,skip_foot=0):
-	#global table
 	table= {}
 	files = os.listdir(directory)
 	nanCounter = 0
-	if skip_head is None:
-		skip_header = 0
-	if skip_head is None:
-		skip_footer = 0
-
 	for x in files:
 		if ext in x:
 			try:
 				table[x[:-len(ext)]] = np.genfromtxt(directory+'\\'+x, delimiter = delimiter, skip_header = skip_head, skip_footer = skip_foot)
 			except:
 				print('Some error occured while doing stuff to '  + x)
-				
+
 		try:		
 			if 'nan' in str(table[x[:-len(ext)]]):
 				print('Something is returning nan in ' + x)
