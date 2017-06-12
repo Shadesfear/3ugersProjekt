6d68a891caf76a5d1bc16e6e76993b534ddc0511
Alexander Schierbeck-Hansen <bvq981@alumni.ku.dk>
2017-06-12 16:00:55 +0200

    Change of model for calculating lambda

--- Projekt.py
+++ Projekt.py
@@ -2,13 +2,18 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import sys
 from fileImport import fileImport
 from detect_peaks import detect_peaks
 from movingaverage import movingaverage
 import math
 import scipy.fftpack
 
+
 ################################################################################
+# The method to use
+method = input('Please Choose "fourier" or "peaks": ')
+
 #Dict to store data
 table = {}
 
@@ -35,9 +40,10 @@ n1 = 1.0
 n2 = 1.333
 r = ((n2 - n1)/(n1 + n2)) ** 2
 
+
 ################################################################################
 # Main code for finding lambda etc
-def findlambda2(expnr, measnr):
+def findlambda2(expnr, measnr, showgraphs, method): # type = 'fourier' or 'peaks'
 	# Find the absorption-spectrum for experiment NMM
 	if len(str(measnr)) < 2:
 		exp = 'forsoeg' + str(expnr) + str(0) + str(measnr)
@@ -47,45 +53,49 @@ def findlambda2(expnr, measnr):
 	a = I0 * (1 - r)**2 / (1 - r**2) - I # absorption
 	A = movingaverage(a, 100, 3) # absorption (movingaverage to smooth out the function)
 
-	# Find peaks
-	peakIndices = detect_peaks(A, mph=10, mpd=100)
-	peaks = []
-	for peakIndex in peakIndices:
-		if (table[exp][peakIndex,0] > 400 and table[exp][peakIndex,0] < 1000):
-			peaks.append(table[exp][peakIndex,0])
-	# print(peaks)
+	if method == 'peaks':
+		# Find peaks
+		peakIndices = detect_peaks(A, mph=np.mean(A), mpd=100)
+		peaks = []
+		for peakIndex in peakIndices:
+			if (table[exp][peakIndex,0] > 400 and table[exp][peakIndex,0] < 1000):
+				peaks.append(table[exp][peakIndex,0])
+		# print(peaks)
+		if showgraphs == 'showgraphs':
+			# Figure with results
+			plt.plot(table[exp][:,0], a, 'b-', label='fit')
+			plt.plot(table[exp][:,0], A, 'r-', label='fit')
+			for peak in peaks:
+				plt.axvline(x=peak)
+			plt.show()
+		# Find lambda2 (inside the film) in [nm]
+		if len(peaks) > 1:
+			m = abs((min(peaks) - max(peaks) * (1 + (2 * (len(peaks) - 1)))) / (2 * (max(peaks) - min(peaks))))
+			lambda1 = (m / 2) * min(peaks)
+			lambda2 = lambda1 * ( n1 / n2 )
+		else:
+			lambda2 = math.nan
+		return lambda2
 
-	# # Figure with results
-	# plt.plot(table[exp][:,0], a, 'b-', label='fit')
-	# plt.plot(table[exp][:,0], A, 'r-', label='fit')
-	# for peak in peaks:
-	# 	plt.axvline(x=peak)
-	# plt.show()
-	N = len(table[exp][:,0])
-	T = (table[exp][-1,0] - table[exp][0,0]) / N # spacing between points (lambdas)
-	freqs = np.linspace(0.0, 1.0 / (2.0 * T), N / 2.0)
-	A_fourier = scipy.fftpack.fft(A)
-	a_fourier = scipy.fftpack.fft(a)
+	if method == 'fourier':
+		N = len(table[exp][:,0])
+		T = (table[exp][-1,0] - table[exp][0,0]) / N # spacing between points (lambdas)
+		freqs = np.linspace(0.0, 1.0 / (2.0 * T), N / 2.0)
+		A_fourier = scipy.fftpack.fft(A)
+		a_fourier = scipy.fftpack.fft(a)
 
-	# plt.plot(freqs, 2.0/N * np.abs(A_fourier[:N//2]), 'b.')
-	# plt.show()
+		if showgraphs == 'showgraphs':
+			plt.plot(freqs, 2.0/N * np.abs(A_fourier[:N//2]), 'b.')
+			plt.show()
 
-	# maxfreqIndex = a_fourier.index(max(a_fourier[2:]))
-	maxfreqIndex = np.argmax(A_fourier[2:len(A_fourier)//2])
-	maxfreq = freqs[maxfreqIndex]
-	# print(maxfreq)
+		maxfreqIndex = 2 + np.argmax(a_fourier[2:len(a_fourier)//2])
+		# maxfreqIndex = 2 + np.argmax(A_fourier[2:len(A_fourier)//2])
+		maxfreq = freqs[maxfreqIndex]
+		# print(maxfreq)
 
-	# Find lambda2 (inside the film) in [nm]
-	# if len(peaks) > 1:
-	# 	lambda1 = (max(peaks) - min(peaks)) / ( len(peaks) - 1)
-	# 	lambda2 = lambda1 * ( n1 / n2 )
-	# else:
-	# 	lambda2 = math.nan
-
-	lambda1 = (table[exp][-1,0] - table[exp][0,0]) / maxfreqIndex # missing a factor of 2 cf def. of freqs?
-	lambda2 = lambda1 * (n1 / n2)
-
-	return lambda2
+		lambda1 = (table[exp][-1,0] - table[exp][0,0]) / maxfreqIndex # missing a factor of 2 cf def. of freqs?
+		lambda2 = lambda1 * (n1 / n2)
+		return lambda2
 
 ################################################################################
 lambda2 = {}
@@ -95,7 +105,7 @@ for i in range(1, 7+1):
 		# Following line filters out the beginning measurements (where the beaker was blocking the signal)
 		if not ((i == 1 and j < 7) or (i == 2 and j < 16) or (i == 3 and j < 12) or (i == 4 and j < 9)
 		or (i == 5 and j < 12) or (i == 6 and j < 11) or (i == 7 and j < 12)):
-			lambda2[str(i)].append(findlambda2(i,j))
+			lambda2[str(i)].append(findlambda2(i, j, 'noshowgraphs', method))
 		else:
 			lambda2[str(i)].append(math.nan)
 		j += 1
