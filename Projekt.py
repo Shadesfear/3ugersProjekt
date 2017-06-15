a305596862d85ca345c2067534d584c80864f666
Dakkedalle <mortendalfoss@gmail.com>
2017-06-15 15:23:10 +0200

    fjernet curveFit

--- Projekt.py
+++ Projekt.py
@@ -43,7 +43,7 @@ wi= 40
 Na = 3
 for x in range(1,8,1):
 	plt.figure(x)
-	plt.plot(table['forsoeg456'][:-(wi-1)*Na,0],movingaveragex(arb['forsoeg'+str(x)+'15'],wi,Na),"s",table['forsoeg456'][:-(wi-1)*Na,0],movingaveragex(arb['forsoeg'+str(x)+'30'],wi,Na),"*",table['forsoeg456'][:-(wi-1)*Na,0],movingaveragex(arb['forsoeg'+str(x)+'60'],wi,Na),"x",table['forsoeg456'][:-(wi-1)*Na,0],movingaveragex(arb['forsoeg'+str(x)+'85'],wi,Na),"o")
+	plt.plot(table['forsoeg456'][:-(wi-1)*Na,0],movingaveragex(arb['forsoeg'+str(x)+'20'],wi,Na),"s",table['forsoeg456'][:-(wi-1)*Na,0],movingaveragex(arb['forsoeg'+str(x)+'40'],wi,Na),"*",table['forsoeg456'][:-(wi-1)*Na,0],movingaveragex(arb['forsoeg'+str(x)+'60'],wi,Na),"x",table['forsoeg456'][:-(wi-1)*Na,0],movingaveragex(arb['forsoeg'+str(x)+'80'],wi,Na),"o")
 
 plt.legend()
 
