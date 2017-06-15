368f8c2034d88ae9ca2e376e11b622b7cf4b9cfd
Dakkedalle <mortendalfoss@gmail.com>
2017-06-15 15:38:09 +0200

    ændret plot layout til standarden for artikel

--- Projekt.py
+++ Projekt.py
@@ -42,8 +42,14 @@ def movingaveragex(values, window, N):
 wi= 40
 Na = 3
 for x in range(1,8,1):
-	plt.figure(x)
-	plt.plot(table['forsoeg456'][:-(wi-1)*Na,0],movingaveragex(arb['forsoeg'+str(x)+'20'],wi,Na),"s",table['forsoeg456'][:-(wi-1)*Na,0],movingaveragex(arb['forsoeg'+str(x)+'40'],wi,Na),"*",table['forsoeg456'][:-(wi-1)*Na,0],movingaveragex(arb['forsoeg'+str(x)+'60'],wi,Na),"x",table['forsoeg456'][:-(wi-1)*Na,0],movingaveragex(arb['forsoeg'+str(x)+'80'],wi,Na),"o")
+	plt.figure(x, figsize=(3.5, 2.8), )
+	plt.plot(table['forsoeg456'][:-(wi-1)*Na,0],movingaveragex(arb['forsoeg'+str(x)+'20'],wi,Na),"s",table['forsoeg456'][:-(wi-1)*Na,0],movingaveragex(arb['forsoeg'+str(x)+'40'],wi,Na),"*",table['forsoeg456'][:-(wi-1)*Na,0],movingaveragex(arb['forsoeg'+str(x)+'60'],wi,Na),"x",table['forsoeg456'][:-(wi-1)*Na,0],movingaveragex(arb['forsoeg'+str(x)+'80'],wi,Na),"o", markersize = 1.1)
+	plt.xlabel('Wavelength [nm]', fontsize = 8)
+	plt.ylabel('Intensity [n]', fontsize = 8)
+	plt.xticks(fontsize = 6)
+	plt.yticks(fontsize = 6, rotation = 'vertical')
+	plt.tight_layout()
+
 
 plt.legend()
 
